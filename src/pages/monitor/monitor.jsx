a62e239095422a98a36b993f8e7877d638f481b6
张森 <zsdota2@sina.com>
2022-08-16 22:08:02 +0800

    feat: 滚动实验

--- src/pages/monitor/monitor.jsx
+++ src/pages/monitor/monitor.jsx
@@ -1,15 +1,26 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import styles from './monitor.less';
 
 const Monitor = () => {
+  const [leftTop, setLeftTop] = useState();
+  const [rightTop, setRightTop] = useState();
   const leftRef = useRef();
+  const rightRef = useRef();
 
   const so = () => {
-    let scrollTop = document.documentElement.scrollTop;
-    let opacity = scrollTop * 0.085;
+    let scrollTop = leftRef.current.getBoundingClientRect().top;
+    let scrollTop2 = rightRef.current.getBoundingClientRect().top;
+    let opacity = scrollTop / leftTop;
+    let opacity2 = scrollTop2 / rightTop;
     leftRef.current.style.opacity = opacity;
-    console.log(opacity);
+    rightRef.current.style.opacity = opacity2;
   };
+  useEffect(() => {
+    let num = leftRef.current.getBoundingClientRect().top;
+    let num2 = rightRef.current.getBoundingClientRect().top;
+    setLeftTop(num);
+    setRightTop(num2);
+  }, []);
   useEffect(() => {
     window.addEventListener('scroll', so);
   }, []);
@@ -17,9 +28,9 @@ const Monitor = () => {
   return (
     <div className={styles.monitor}>
       <div className={styles.left} ref={leftRef}>
-        1
+        123
       </div>
-      <div className={styles.right}>2</div>
+      <div className={styles.right} ref={rightRef} />
     </div>
   );
 };
